98b1cac9f199a4501a798cc84452a8dfae981c46
ahah084 <67080701+JHOW157@users.noreply.github.com>
2025-08-11 22:41:10 -0300

    Update ahah.lua

--- ahah.lua
+++ ahah.lua
@@ -115,12 +115,12 @@ imgui.OnFrame(function() return GUI.AbrirMenu[0] end, function()
             GUI.selected_category = "visual"
             playSoundAtPlayerLocation()
         end
-        imgui.Dummy(imgui.ImVec2(0, 160 * DPI))
+        imgui.Dummy(imgui.ImVec2(0, 150 * DPI))
         if imgui.Button(     faicons("GEAR") .. " CONFIG         ", categoria) then
             GUI.selected_category = "config"
             playSoundAtPlayerLocation()
         end
-        imgui.Dummy(imgui.ImVec2(0, 2 * DPI))
+        imgui.Dummy(imgui.ImVec2(0, 5 * DPI))
 
         imgui.EndChild()
         imgui.SameLine()
