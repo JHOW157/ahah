ff78952ede77a78ebe8530c86bf89bbdc6389977
ahah084 <67080701+JHOW157@users.noreply.github.com>
2025-08-13 20:20:45 -0300

    Update ahah.lua

--- ahah.lua
+++ ahah.lua
@@ -48,7 +48,7 @@ local GUI = {
     EspCarro = new.bool(false),
     AtivarMessagesLog = new.bool(false),
     AtivarTelaEsticada = new.bool(false),
-    AlterarFovTela = new.int(60),
+    AlterarFovTela = new.int(70),
     selected_category = "creditos"
 }
 
@@ -128,7 +128,7 @@ imgui.OnFrame(function() return GUI.AbrirMenu[0] end, function()
             GUI.selected_category = "visual"
             playSoundAtPlayerLocation()
         end
-        imgui.Dummy(imgui.ImVec2(0, 85 * DPI))
+        imgui.Dummy(imgui.ImVec2(0, 80 * DPI))
         if imgui.Button(     faicons("GEAR") .. " CONFIG         ", categoria) then
             GUI.selected_category = "config"
             playSoundAtPlayerLocation()
